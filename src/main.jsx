71152be531bc2bfd85e07bea913711f7797fe67a
Marufhossain07 <marufprogrammer07@gmail.com>
2024-04-26 20:51:04 +0600

    error file added

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import Home from './layouts/Home/Home.jsx';
+import Error from './layouts/Error.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>
-  },
+    element: <Home></Home>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+
+      }
+    ]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
