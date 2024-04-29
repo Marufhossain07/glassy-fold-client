c96fc3fec3862b5fbee6ca435e35cbf9bbb36867
Marufhossain07 <marufprogrammer07@gmail.com>
2024-04-29 23:19:53 +0600

    show details page added

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-# React + Vite
+# GlassyFold
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Live : 
 
-Currently, two official plugins are available:
+Welcome to Artistic Creations! Your go-to destination for exquisite handcrafted art pieces and creative inspirations.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Key Features:
+- Diverse Collection: Explore a diverse range of art and craft items including paintings, sculptures, pottery, and more, each crafted by talented artisans.
+- Custom Creations: Personalize your art pieces with customizations tailored to your preferences, ensuring a truly unique and meaningful creation.
+- Inspiration Gallery: Get inspired by our curated gallery showcasing stunning art pieces and DIY craft projects, perfect for unleashing your creativity.
+- Artisan Spotlight: Discover the stories behind the artisans and makers who bring our art pieces to life, celebrating their craftsmanship and dedication.
+- Secure Shopping: Shop with confidence knowing that your transactions are secure, and your information is protected.
+
+
+Website Category: 
+- Paper Crafts & Glass Art
+
+Sub-Category:
+
+ - CardMaking
+ - Scrapbooking
+ - Paper Quilling & origami
+ - Glass Painting
+ - Lampworking
+ - Glass Dying & Staining
--- src/layouts/Details.jsx
+++ src/layouts/Details.jsx
@@ -1,8 +1,35 @@
+import { useLoaderData } from "react-router-dom";
 
 const Details = () => {
+    const data = useLoaderData();
+    const { item, sub, description, price, rating, customization, time, stock, email, name, photo } = data;
     return (
-        <div>
-            <h3>hi</h3>
+        <div className="mt-5">
+            <h3 className="text-4xl font-pop text-center 
+            font-semibold text-[#2b2d42]">Item Details</h3>
+            <div className="text-[#2b2d42] flex gap-5 mt-10 border-2 border-[#2b2d42] rounded-lg">
+                <div>
+                    <img className="rounded-lg h-96" src={photo} alt="" />
+                </div>
+                <div>
+                    <h3 className="text-2xl font-pop font-semibold py-3">Name: {item}</h3>
+                    <p className="text-lg font-medium pb-10">Description: {description}</p>
+                    <div className="flex gap-10">
+                        <h3 className="text-lg font-medium">Price : {price}</h3>
+                        <h3 className="text-lg font-medium">Rating : {rating}</h3>
+                    </div>
+
+                    <p className="text-base font-medium pt-3">Sub-Category : {sub}</p>
+                    <p className="text-base font-medium pt-3">Customization : {customization}</p>
+
+                    <div>
+                        <p className="text-base font-medium pt-3">Processing Time : {time}</p>
+                        <p className="text-base font-medium pt-3">Stock : {stock}</p>
+
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     );
 };
